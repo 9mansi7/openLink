ec3e482c47c73b2a9b15c45e0310705a9bf36fa8
Mansi Singh <mansisingh090701@gmail.com>
2025-09-25 21:15:21 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Container from "./container/Container.jsx";
+import Logo from "./logo.jsx";
+import Logout_btn from "./logout_btn.jsx";
+import Button from "./Button.jsx";
+import Input from "./input.jsx";
+import Select from "./select";
+import PostCard from "./PostCard.jsx";
+import RTE from './RTE.jsx';
+import Protected from './AuthLayout.jsx'
+import Login  from './login.jsx';
+import Signup from './signup.jsx';
+import PostForm from "./post-form/PostForm.jsx";
+
+
+export { Header, Footer, Container, Logo, Logout_btn, Button, Input, Select, PostCard, RTE, Protected, Login, Signup, PostForm };
